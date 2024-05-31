2abe9948c3d327bd4e98a9166d37b7759219ab34
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-05-31 12:26:01 +0300

    рабочая тестовая версия 3

--- testAI.py
+++ testAI.py
@@ -4,12 +4,12 @@ from openai import OpenAI
 import telebot
 
 # OpenAI API key
-client = OpenAI(api_key='ВАШ ТОКЕН',
+client = OpenAI(api_key='sk-SeA46H6qerPo06VdVK2HxMabiqq7maWT',
                 base_url="https://api.proxyapi.ru/openai/v1")
 
 
 # Telegram bot token
-bot = telebot.TeleBot("ВАШ ТОКЕН")
+bot = telebot.TeleBot("6593004563:AAF6_VdpPQKQ-hYjsAcrz6GBwuVnx_CPwFc")
 
 # Function to get a random question from the database
 def get_random_question():
@@ -30,9 +30,10 @@ def get_feedback(question, user_response):
     prompt = f"""Действуй как опытный HR-программист, специалист по пайтону.
     Ты получил вопрос: "{question}"
     и ответ соискателя: "{user_response}".
-    Ты должен оценить ответ и дать обратную связь. Твоя оценка должна начинаться только с 'правильный ответ', если ответ правильный, или 'не правильный ответ', если ответ неправильный или неполный. Если ответ не правильный, потом напиши правильный ответ. 
+    Ты должен оценить ответ и дать обратную связь. Есть два варианта начала ответа : 'правильный ответ', если ответ правильный, или 'не правильный ответ', если ответ неправильный или неполный. Если ответ не правильный, потом напиши правильный ответ. 
     Твоя оценка имеет такую структуру : Правильный/не правильный ответ , потом правильный ответ или , если ответ правильный маленькие добавления."""
-       
+
+
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-16k-0613",
         messages=[
